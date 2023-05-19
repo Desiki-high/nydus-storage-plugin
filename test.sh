0b2e771d6b4aa57588476f611427fcb2da16a287
Yadong Ding <ding_yadong@foxmail.com>
2023-05-19 12:51:45 +0800

    test
    Signed-off-by: Yadong Ding <ding_yadong@foxmail.com>

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 sudo podman run -it ghcr.io/dragonflyoss/image-service/nginx:nydus-latest echo hello word
 sudo podman images
 sudo podman pull ghcr.io/dragonflyoss/image-service/alpine:nydus-latest
+sudo podman info
